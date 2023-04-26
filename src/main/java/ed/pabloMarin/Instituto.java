ee228641a53728dd53554efe79fc949995543d07
Pablofasl <pablox2004x@gmail.com>
2023-04-26 19:37:18 +0200

    Constantes de (nombre, apellidos, ciclo y año) creadas | pabloMarin

--- src/main/java/ed/pabloMarin/Instituto.java
+++ src/main/java/ed/pabloMarin/Instituto.java
@@ -2,17 +2,23 @@ package ed.pabloMarin;
 
 public class Instituto {
 
+	public static final String kNOMBRE_ALUMNO = "Camilo";
+
+	public static final String kAPELLIDOS_ALUMNO = "Juan";
+
+	public static final String kCICLO_FORMATIVO = "1º DAM/DAW";
+
+	public static final int kANYO = 2023;
+
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
 		
-		p.nombre = "Camilo";
-		p.apellidos = "Juan";
-		p.cicloFormativo = "1º DAM/DAW";
-		p.anyo = 2023;
+		p.nombre = kNOMBRE_ALUMNO;
+		p.apellidos = kAPELLIDOS_ALUMNO;
+		p.cicloFormativo = kCICLO_FORMATIVO;
+		p.anyo = kANYO;
 		
 		System.out.println(p.toString());
-
 	}
-
 }
