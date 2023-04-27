d0334778488808856a6b32c06e26943176dc2a54
IzanBlanes <izanblanesaracil@gmail.com>
2023-04-27 21:06:16 +0200

    Incluidas las constantes del nombre, apellidos, ciclo formativo y el año para los atributos públicos de la clase Alumno en la clase Instituto

--- src/main/java/ed/izan/Instituto.java
+++ src/main/java/ed/izan/Instituto.java
@@ -2,14 +2,19 @@ package ed.izan;
 
 public class Instituto {
 
+	public static final String NOMBRE_ALUMNO = "Izan";
+	public static final String APELLIDOS_ALUMNO = "Blanes";
+	public static final String CICLO_FORMATIVO = "1º DAM";
+	public static final int ANYO = 2023;
+
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
 		
-		p.nombre = "Izan";
-		p.apellidos = "Blanes";
-		p.cicloFormativo = "1º DAM";
-		p.anyo = 2023;
+		p.nombre = NOMBRE_ALUMNO;
+		p.apellidos = APELLIDOS_ALUMNO;
+		p.cicloFormativo = CICLO_FORMATIVO;
+		p.anyo = ANYO;
 		
 		System.out.println(p.toString());
 
