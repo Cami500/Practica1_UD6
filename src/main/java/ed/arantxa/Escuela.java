87be16417e11a729cb6a2ce169f824520058ac99
Arantxa Gandia <gandiaarantxa@gmail.com>
2023-04-27 19:11:37 +0200

    [ADD] Punto 1

--- src/main/java/ed/arantxa/Escuela.java
+++ src/main/java/ed/arantxa/Escuela.java
@@ -1,4 +1,4 @@
-package ed.camilo;
+package ed.arantxa;
 
 public class Escuela {
 
@@ -6,9 +6,9 @@ public class Escuela {
 		
 		Persona p = new Persona();
 		
-		p.n = "Camilo";
-		p.c = "Juan";
-		p.cl = "1º DAM/DAW";
+		p.n = "Arantxa";
+		p.c = "Gandia";
+		p.cl = "1º DAW";
 		p.a = 2023;
 		
 		System.out.println(p.toString());
