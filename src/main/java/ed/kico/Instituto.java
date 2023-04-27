0e5e77b3aa9d0645c94725567e7a7c3fb0ee5a9c
Kokon44 <fracargar@alu.edu.gva.es>
2023-04-27 19:38:30 +0200

    3A

--- src/main/java/ed/kico/Instituto.java
+++ src/main/java/ed/kico/Instituto.java
@@ -2,6 +2,10 @@ package ed.kico;
 
 public class Instituto {
 
+	public static String kNOMBRE_ALUMNO = "Kico";
+	public static String kAPELLIDO_ALUMNO = "Carbonell";
+	public static String kCICLO_FORMATIVO = "1º DAW";
+	public static int kANYO = 2023;
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
