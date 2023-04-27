8142e33a5260dd2714f3ea5a44c1d49491d05bfd
LucasJR13 <lucjuariq@alu.edu.gva.es>
2023-04-27 19:21:34 +0200

    [ADD] Ejercicio 3

--- src/main/java/ed/lucas/Instituto.java
+++ src/main/java/ed/lucas/Instituto.java
@@ -2,14 +2,19 @@ package ed.lucas;
 
 public class Instituto {
 
+	public static final String kNOMBRE_ALUMNO = "Lucas";
+	public static final String kAPELLIDOS_ALUMNO = "Juan";
+	public static final String kCICLO_FORMATIVO = "1º DAW";
+	public static final int kANYO = 2023;
+
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
 		
-		p.nombre = "Lucas";
-		p.apellidos = "Juan";
-		p.cicloFormativo = "1º DAW";
-		p.anyo = 2023;
+		p.nombre = kNOMBRE_ALUMNO;
+		p.apellidos = kAPELLIDOS_ALUMNO;
+		p.cicloFormativo = kCICLO_FORMATIVO;
+		p.anyo = kANYO;
 		
 		System.out.println(p.toString());
 
