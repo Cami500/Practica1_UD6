c50134edce12ecf1250615fcb7dcfb03716d1f98
TATIANA <tatianaypunto@gmail.com>
2023-04-28 16:26:19 +0200

    de cadenas a constantes

--- src/main/java/ed/tatiana/Instituto.java
+++ src/main/java/ed/tatiana/Instituto.java
@@ -2,14 +2,19 @@ package ed.tatiana;
 
 public class Instituto {
 
+	public static final String kNOMBRE_ALUMNO = "Camilo";
+	public static final String kAPELLIDOS_ALUMNO = "Juan";
+	public static final String kCICLO_FORMATIVO = "1º DAM/DAW";
+	public static final int kANYO = 2023;
+
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
 
-		p.nombre = "Camilo";
-		p.apellidos = "Juan";
-		p.ciclo_formativo = "1º DAM/DAW";
-		p.anyo = 2023;
+		p.nombre = kNOMBRE_ALUMNO;
+		p.apellidos = kAPELLIDOS_ALUMNO;
+		p.ciclo_formativo = kCICLO_FORMATIVO;
+		p.anyo = kANYO;
 		
 		System.out.println(p.toString());
 
