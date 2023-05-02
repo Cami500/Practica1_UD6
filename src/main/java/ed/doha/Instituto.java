febe54fd6ca1e286ea9c54ae826e593912839d67
Doha Aliat Belkas <dohaaliat0@gmail.com>
2023-05-02 15:54:47 +0200

    Tarea3

--- src/main/java/ed/doha/Instituto.java
+++ src/main/java/ed/doha/Instituto.java
@@ -2,14 +2,20 @@ package ed.doha;
 
 public class Instituto {
 
+
+	public static final String kNOMBRE_ALUMNO = "Doha";
+	public static final String kAPELLIDOS_ALUMNO = "Aliat";
+	public static final String kCICLO_FORMATIVO = "1º DAW";
+	public static final int kANYO = 2023;
+
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
 		
-		p.nombre = "Doha";
-		p.apellidos = "Aliat";
-		p.cicloInformativo = "1º DAW";
-		p.anyo = 2023;
+		p.nombre = kNOMBRE_ALUMNO;
+		p.apellidos = kAPELLIDOS_ALUMNO;
+		p.cicloInformativo = kCICLO_FORMATIVO;
+		p.anyo = kANYO;
 
 		System.out.println(p.toString());
 
