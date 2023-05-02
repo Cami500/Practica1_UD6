65b8f17a228b569447300b9d5bb6b4490d8a04c4
Andreu Puchades <andpucpas@alu.edu.gva.es>
2023-05-02 15:47:46 +0200

    [MOD] Tarea 3

--- src/main/java/ed/andreu/Instituto.java
+++ src/main/java/ed/andreu/Instituto.java
@@ -1,6 +1,11 @@
 package ed.andreu;
 
 public class Instituto {
+	final static String kNOMBRE_ALUMNO = "Camilo";
+	final static String kAPELLIDOS_ALUMNO = "Juan";
+	final static String kCICLO_FORMATIVO = "1º DAM/DAW";
+	final static int kANYO = 2023;
+
 	public static void main(String[] args) {
 		Alumno p = new Alumno();
 		
