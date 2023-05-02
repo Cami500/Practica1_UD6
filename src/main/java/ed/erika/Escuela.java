0807a70597763ee2e3def45e153d3b86d1841506
erikabp93 <eribalpas@alu.edu.gva.es>
2023-05-02 17:03:47 +0200

    Cambio de nombre en la carpeta y cambio de datos en el main en Escuela

--- src/main/java/ed/erika/Escuela.java
+++ src/main/java/ed/erika/Escuela.java
@@ -1,4 +1,4 @@
-package ed.camilo;
+package ed.erika;
 
 public class Escuela {
 
@@ -6,9 +6,9 @@ public class Escuela {
 		
 		Persona p = new Persona();
 		
-		p.n = "Camilo";
-		p.c = "Juan";
-		p.cl = "1º DAM/DAW";
+		p.n = "Erika";
+		p.c = "Ballesteros";
+		p.cl = "1º DAM";
 		p.a = 2023;
 		
 		System.out.println(p.toString());
