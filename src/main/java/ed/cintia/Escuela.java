a7e128eea57937c7aac2ae39f1c566945d92a2e4
Cintia Cantó <cincanest@alu.edu.gva.es>
2023-05-02 19:26:12 +0200

    Ejercicio 1

--- src/main/java/ed/cintia/Escuela.java
+++ src/main/java/ed/cintia/Escuela.java
@@ -1,4 +1,4 @@
-package ed.camilo;
+package ed.cintia;
 
 public class Escuela {
 
@@ -6,9 +6,9 @@ public class Escuela {
 		
 		Persona p = new Persona();
 		
-		p.n = "Camilo";
-		p.c = "Juan";
-		p.cl = "1º DAM/DAW";
+		p.n = "Cintia";
+		p.c = "Cantó";
+		p.cl = "1º DAW";
 		p.a = 2023;
 		
 		System.out.println(p.toString());
