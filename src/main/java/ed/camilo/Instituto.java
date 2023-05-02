78b5e62c6ce1e03998cf5ddd104dfabf9d42c4a9
Nuria <nubogen@gmail.com>
2023-05-02 21:21:19 +0200

    [ADD] tarea 3

--- src/main/java/ed/camilo/Instituto.java
+++ src/main/java/ed/camilo/Instituto.java
@@ -2,14 +2,19 @@ package ed.camilo;
 
 public class Instituto {
 
+	public static final String kNOMBRE_ALUMNO = "Nuria";
+	public static final String kAPELLIDOS_ALUMNO = "Boronat";
+	public static final String kCICLO_FORMATIVO = "1º DAW";
+	public static final int Kanyo = 2023;
+
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
 		
-		p.nombre = "Nuria";
-		p.apellidos = "Boronat";
-		p.cicloFormativo = "1º DAW";
-		p.anyo = 2023;
+		p.nombre = kNOMBRE_ALUMNO;
+		p.apellidos = kAPELLIDOS_ALUMNO;
+		p.cicloFormativo = kCICLO_FORMATIVO;
+		p.anyo = Kanyo;
 		
 		System.out.println(p.toString());
 
