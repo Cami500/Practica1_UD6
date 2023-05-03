da07a0d5affa99a777d3fbe152c275381091c0f9
erikabp93 <eribalpas@alu.edu.gva.es>
2023-05-03 18:25:03 +0200

    Introduccion de Constantes en el metodo main

--- src/main/java/ed/erika/Instituto.java
+++ src/main/java/ed/erika/Instituto.java
@@ -2,14 +2,19 @@ package ed.erika;
 
 public class Instituto {
 
+	public static final String ERIKA = "Erika";
+	public static final String BALLESTEROS = "Ballesteros";
+	public static final String CICLO_FORMATIVO = "1º DAM";
+	public static final int ANYO = 2023;
+
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
 		
-		p.nombre = "Erika";
-		p.apellidos = "Ballesteros";
-		p.cicloFormativo = "1º DAM";
-		p.anyo = 2023;
+		p.nombre = ERIKA;
+		p.apellidos = BALLESTEROS;
+		p.cicloFormativo = CICLO_FORMATIVO;
+		p.anyo = ANYO;
 		
 		System.out.println(p.toString());
 
