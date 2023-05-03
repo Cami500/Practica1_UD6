640223d308d2dd760ec0e36e1d9e8d9df4ef20c6
Cintia Cantó <cincanest@alu.edu.gva.es>
2023-05-03 10:09:31 +0200

    Ejercicio 3

--- src/main/java/ed/cintia/Instituto.java
+++ src/main/java/ed/cintia/Instituto.java
@@ -2,14 +2,19 @@ package ed.cintia;
 
 public class Instituto {
 
+	public static final String kNOMBRE_ALUMNO = "Cintia";
+	public static final String kAPELLIDOS_ALUMNO = "Cantó";
+	public static final String kCICLO_FORMATIVO = "1º DAW";
+	public static final int kANYO = 2023;
+
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
 		
-		p.nombre = "Cintia";
-		p.apellidos = "Cantó";
-		p.cicloFormativo = "1º DAW";
-		p.anyo = 2023;
+		p.nombre = kNOMBRE_ALUMNO;
+		p.apellidos = kAPELLIDOS_ALUMNO;
+		p.cicloFormativo = kCICLO_FORMATIVO;
+		p.anyo = kANYO;
 		
 		System.out.println(p.toString());
 
