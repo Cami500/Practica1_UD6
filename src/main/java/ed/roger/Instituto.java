27db3d774a077d3320790fadd96c475bc702f781
Roger <mollseguiroger@gmail.com>
2023-05-03 11:55:47 +0200

    Punto 3 realizado

--- src/main/java/ed/roger/Instituto.java
+++ src/main/java/ed/roger/Instituto.java
@@ -2,14 +2,19 @@ package ed.roger;
 
 public class Instituto {
 
+	public static final String kNOMBRE_ALUMNO = "Roger";
+	public static final String kAPELLIDOS_ALUMNO = "Moll";
+	public static final String kCICLO_FORMATIVO = "1º DAM";
+	public static final int kANYO = 2023;
+
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
 		
-		p.nombre = "Roger";
-		p.apellido = "Moll";
-		p.cicloFormativo = "1º DAM";
-		p.anyo = 2023;
+		p.nombre = kNOMBRE_ALUMNO;
+		p.apellido = kAPELLIDOS_ALUMNO;
+		p.cicloFormativo = kCICLO_FORMATIVO;
+		p.anyo = kANYO;
 		
 		System.out.println(p.toString());
 
