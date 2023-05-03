5527fcd117abc1dd594f70492fdf965ea4c6d12a
Joan Company <joancompanyrichart@gmail.com>
2023-05-03 16:36:23 +0200

    Commit_tercer_ejercicion

--- src/main/java/ed/joan/Instituto.java
+++ src/main/java/ed/joan/Instituto.java
@@ -2,17 +2,22 @@ package ed.joan;
 
 public class Instituto {
 
+	public static final String kNOMBRE_ALUMNO = "Camilo";
+	public static final String kAPELLIDOS_ALUMNO = "Juan";
+	public static final String kCICLO_FORMATIVO = "1º DAM/DAW";
+	public static final int kANYO = 2023;
+
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
 
-		String camilo = "Camilo";
+		String camilo = kNOMBRE_ALUMNO;
 		p.nombre = camilo;
-		String juan = "Juan";
+		String juan = kAPELLIDOS_ALUMNO;
 		p.apellidos = juan;
-		String cl = "1º DAM/DAW";
+		String cl = kCICLO_FORMATIVO;
 		p.ciclo_formativo = cl;
-		int a = 2023;
+		int a = kANYO;
 		p.anyo = a;
 		
 		System.out.println(p.toString());
