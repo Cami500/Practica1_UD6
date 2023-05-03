3e976106c75be707fcd367e26897a7bdb492035a
Jaume <jmg180802@gmail.com>
2023-05-03 11:56:21 +0200

    Tercer punto realizado

--- src/main/java/ed/jaume/Instituto.java
+++ src/main/java/ed/jaume/Instituto.java
@@ -2,14 +2,19 @@ package ed.jaume;
 
 public class Instituto {
 
+	public static final String kNOMBRE_ALUMNO = "Jaume";
+	public static final String kAPELLIDOS_ALUMNO = "Moltó";
+	public static final String kCICLO_FORMATIVO = "1º DAM";
+	public static final int kANYO = 2023;
+
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
 		
-		p.nombre = "Jaume";
-		p.apellido = "Moltó";
-		p.cicloformativo = "1º DAM";
-		p.anyo = 2023;
+		p.nombre = kNOMBRE_ALUMNO;
+		p.apellido = kAPELLIDOS_ALUMNO;
+		p.cicloformativo = kCICLO_FORMATIVO;
+		p.anyo = kANYO;
 		
 		System.out.println(p.toString());
 
