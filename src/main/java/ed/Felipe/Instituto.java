b13bbad0e35fa3fc10c4891176233b52642d51c0
Felizote <felizotefil@gmail.com>
2023-05-03 11:25:16 +0200

    Extraidas cadenas y numeros a constantes

--- src/main/java/ed/Felipe/Instituto.java
+++ src/main/java/ed/Felipe/Instituto.java
@@ -2,17 +2,19 @@ package ed.Felipe;
 
 public class Instituto {
 
+	public static final String k_NOMBRE_ALUMNO = "Camilo";
+	public static final String k_APELLIDOS_ALUMNO = "Juan";
+	public static final String k_CICLO_FORMATIVO = "1º DAM/DAW";
+	public static final int kANYO = 2023;
+
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
 
-		String camilo = "Camilo";
-		p.nombre = camilo;
-		String juan = "Juan";
-		p.apellidos = juan;
-		String cl = "1º DAM/DAW";
-		p.ciclo_formativo = cl;
-		p.anyo = 2023;
+		p.nombre = k_NOMBRE_ALUMNO;
+		p.apellidos = k_APELLIDOS_ALUMNO;
+		p.ciclo_formativo = k_CICLO_FORMATIVO;
+		p.anyo = kANYO;
 		
 		System.out.println(p.toString());
 
