62670fafafd7b4a2233f5b1159c46297fb41e96e
Alvaro Lucas <alvarolucasperez@gmail.com>
2023-05-04 19:20:37 +0200

    [MOD] Aparatado 3

--- src/main/java/ed/alvaro/Instituto.java
+++ src/main/java/ed/alvaro/Instituto.java
@@ -2,14 +2,19 @@ package ed.alvaro;
 
 public class Instituto {
 
+	public static final String kNOMBRE_ALUMNO = "Alvaro";
+	public static final String kAPELLIDOS_ALUMNO = "Lucas";
+	public static final String kCICLO_FORMATIVO = "1º DAW";
+	public static final int kANYO = 2023;
+
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
 		
-		p.nombre = "Alvaro";
-		p.apellidos = "Lucas";
-		p.cicloFormativo = "1º DAW";
-		p.anyo = 2023;
+		p.nombre = kNOMBRE_ALUMNO;
+		p.apellidos = kAPELLIDOS_ALUMNO;
+		p.cicloFormativo = kCICLO_FORMATIVO;
+		p.anyo = kANYO;
 		
 		System.out.println(p.toString());
 
