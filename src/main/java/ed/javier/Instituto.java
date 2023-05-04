01c75078986686cfeee16265dc977ac2b8ad0f90
Javier Guijarro <JavGuiVr>
2023-05-04 10:18:53 +0200

    Ejercicio 3 finalizado

--- src/main/java/ed/javier/Instituto.java
+++ src/main/java/ed/javier/Instituto.java
@@ -2,14 +2,20 @@ package ed.javier;
 
 public class Instituto {
 
+	final static String kNOMBRE_ALUMNO = "Javier";
+	final static String kAPELLIDOS_ALUMNO = "Guijarro";
+	final static String kCICLO_FORMATIVO = "1º DAW";
+	final static int kANYO = 2023;
+
+
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
 		
-		p.nombre = "Javier";
-		p.apellidos = "Guijarro";
-		p.cicloFormativo = "1º DAW";
-		p.anyo = 2023;
+		p.nombre = kNOMBRE_ALUMNO;
+		p.apellidos = kAPELLIDOS_ALUMNO;
+		p.cicloFormativo = kCICLO_FORMATIVO;
+		p.anyo = kANYO;
 		
 		System.out.println(p.toString());
 
