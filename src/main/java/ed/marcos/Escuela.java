fe5604c80705b78e5ae641c58c8f632fb7fb1604
Marcos Sanz <marcos4sanz@gmail.com>
2023-05-04 20:00:45 +0200

    Parte 1

--- src/main/java/ed/marcos/Escuela.java
+++ src/main/java/ed/marcos/Escuela.java
@@ -1,4 +1,4 @@
-package ed.camilo;
+package ed.marcos;
 
 public class Escuela {
 
@@ -6,8 +6,8 @@ public class Escuela {
 		
 		Persona p = new Persona();
 		
-		p.n = "Camilo";
-		p.c = "Juan";
+		p.n = "Marcos";
+		p.c = "Sanz";
 		p.cl = "1º DAM/DAW";
 		p.a = 2023;
 		
--- src/main/java/ed/marcos/Persona.java
+++ src/main/java/ed/marcos/Persona.java
@@ -1,4 +1,4 @@
-package ed.camilo;
+package ed.marcos;
 
 public class Persona {
 	
