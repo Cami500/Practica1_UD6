41341ac7dcdcba3061e9970db56cf10587fef8dd
Marcos Sanz <marcos4sanz@gmail.com>
2023-05-04 20:04:40 +0200

    Parte 3

--- src/main/java/ed/marcos/Instituto.java
+++ src/main/java/ed/marcos/Instituto.java
@@ -2,14 +2,19 @@ package ed.marcos;
 
 public class Instituto {
 
+	public static final String kNOMBRE_ALUMNO = "Marcos";
+	public static final String kAPELLIDOS_ALUMNO = "Sanz";
+	public static final String kCICLO_FORMATIVO = "1º DAM/DAW";
+	public static final int kANYO = 2023;
+
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
 		
-		p.nombre = "Marcos";
-		p.apellidos = "Sanz";
-		p.cicloFormativo = "1º DAM/DAW";
-		p.anyo = 2023;
+		p.nombre = kNOMBRE_ALUMNO;
+		p.apellidos = kAPELLIDOS_ALUMNO;
+		p.cicloFormativo = kCICLO_FORMATIVO;
+		p.anyo = kANYO;
 		
 		System.out.println(p.toString());
 
